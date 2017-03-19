af2046968db86c2ab46035a5ac1ec19c2761e1ea
Jameson Treu <jamesontreu@gmail.com>
2017-03-19 00:12:51 -0500

    prevented duplicate activities on dialog

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,12 @@
 <template>
-  <!-- App -->
   <div id="app">
-
-    <!--
-    Use the sl-entry-dialog component.
-
-    :open means we are passing data to the property (aka prop) called 'open' that's defined in this component.
-
-    Since dialogIsOpen is just some data.. we can pass it. Vuejs handles all the propogation stuff when dialogIsOpen
-    changes. So for instance, when dialogIsOpen changes to true or false, it will be updated in the sl-entry-dialog component.
-
-    'entry' is yet another prop. If you look in the EntryDialog.vue component you'll see that it is expected to be of type Object.
-    Again. Objects in javascript are just key:value pairs.
-
-    For example:
-
-    {
-      'isCool': true,
-      'isOpen': true,
-      'initWorld': function () { console.log('Hello World') },
-      'anotherObject': {
-        'anotherKey': 'anotherValue',
-        'anArray': [
-          'stuff',
-          'stuff2',
-          'stuff3',
-          5,
-          6
-          ]
-      }
-    }
-
-    keys must be strings. However, values can be whatever.
-
-    v-on:add allows us to listen to events that the component will propogate up to the parent (this is the parent component).
-    In this case, we are listening to the 'add' event. In the component, when this.$emit('add') occurs, then we can know about it by using
-    v-on:add="doSomething()".
-
-    v-on:remove is a way for us to handle when the child component emits an even called 'remove'. Keep in mind these names are arbitrary. You
-    could call it 'stuff' or whatever. So if you wanted to listen to the event 'stuff' you would do
-
-    v-on:stuff="" // in the string you call a function you want to do something in response to the stuff. For example
-
-    v-on:stuff="console.log('mmmm mmm stuff')"
-
-    You also notice the $event that we pass. If the child component passes an argument with their event emission:
-
-    this.$emit('stuff', someArgument, someArgument2)
-
-    Then we can use $event to get those arguments. In this case we are getting the index that is passed up and calling
-    the removeActivity() function that we've defined in our methods object.
-
-    v-on:close allows us to handle the emission of an event called 'close'
-    -->
     <sl-entry-dialog class="dialog"
       :open="dialogIsOpen"
       :entry="activeEntry"
+      :default-activites="defaultActivites"
       v-on:add="addActivity()"
       v-on:remove="removeActivity($event)"
       v-on:close="closeDialog()"></sl-entry-dialog>
-
-    <!--
-    A Table where we display our stuff
-    -->
     <table class="table">
       <tr>
         <th>DATE:</th>
@@ -77,83 +21,23 @@
 </template>
 
 <script>
-/*
-Delete these comments later after you've understood how things are connecting to one another
-*/
-/*
-Import other components you want to use
-*/
 import EntryDialog from './components/EntryDialog'
 
 export default {
-  // Name the component (required)
   name: 'app',
-  // If you use components in other files, reference them here
   components: {
-    /*
-    In our markup, we'll reference EntryDialog as 'sl-entry-dialog', but you can call it whatever.. just stay consistent.
-
-    Staying consistent means if you call this 'sl-whatever-component' then in your markup you will need to write
-
-    <sl-whatever-component></sl-whatever-component>
-    */
     'sl-entry-dialog': EntryDialog
   },
   data () {
     return {
-      /*
-      entries is an object that contains the entries for each day and hour. The object is structured as follows:
-
-      entries: {
-        'todaysDate': {
-          '1am': {
-            date: 'todaysDate',
-            time: '1am',
-            activities: [{
-              name: 'Sleep',
-              duration: 60 // measured in minutes
-            }, {
-              name: 'Caffeine',
-              duration: 60 // You said you don't want this so then you will have to think about how to restructure this object to fit your needs
-            }]
-          },
-          '2am': { ... },
-          '3am': { ... }, ...
-        },
-        'tomorrowsDate': { ... },
-        'dateAfterThat': { ... }, ...
-      }
-      */
       entries: {},
-
-      /*
-      times is an array that holds the times that we'll display with v-for in our markup
-      */
       times: [],
-
-      /*
-      dates is an array that holds the dates that we'll display with v-for in our markup
-      */
       dates: [],
-
-      /*
-      dialogIsOpen is a boolean that we toggle to display the dialog or not
-      */
       dialogIsOpen: false,
-
-      /*
-      activeEntry is the current entry that's being modified in the dialog / modal .. this will be set
-      when we open the dialog.. (see the openDialog method below)
-      */
-      activeEntry: { date: '', time: '', activities: [] }
+      activeEntry: { date: '', time: '', activities: [] },
+      defaultActivites: ['Sleep', 'Caffeine', 'Medicine']
     }
   },
-  /*
-  This is a lifecycle hook for vuejs components. There are other lifecycle hooks.
-
-  Within the mounted() lifecycle hook we can do some setup. In this case, we're setting up
-  the array of times and dates that we will use in the the markup with v-for.
-  */
   mounted () {
     // Initialize times array
     this.generateTimes()
@@ -164,34 +48,14 @@ export default {
     // Initialize entries object
     this.initEntries()
   },
-  /*
-  methods are functions that our component can use to do things
-  */
   methods: {
-    /*
-    initEntries initializes the entries object that we use to store the data of each entry in the table.
-    */
     initEntries () {
-      /*
-      Initialize entries object
-      */
       var entries = {}
-
-      /*
-      Have a default activity for each entry so each entry's dialog will display at least 1 activity to start with.
-
-      Activities are displayed in EntryDialog.vue component.
-      */
       var defaultActivity = {
         name: 'Sleep',
-        duration: 30
+        isDone: false
       }
 
-      /*
-      For each date
-        For each time of the day
-          create an entry for that day and time and set date, time and activities for that day/time entry
-      */
       for (var i = 0; i < this.dates.length; i++) {
         entries[`${this.dates[i]}`] = {}
         for (var j = 0; j < this.times.length; j++) {
@@ -203,14 +67,8 @@ export default {
         }
       }
 
-      /*
-      Set out component's entries data to the entries that we've created here
-      */
       this.entries = entries
     },
-    /*
-    generateTimes intitializes the times array that will be used to display the times columns
-    */
     generateTimes () {
       var am = []
       var pm = []
@@ -225,9 +83,6 @@ export default {
 
       this.times = am.concat(pm)
     },
-    /*
-    generateDates initializes the dates array that will be used to display the dates rows
-    */
     generateDates () {
       var dates = [new Date()]
 
@@ -242,52 +97,42 @@ export default {
       }
       this.dates = dates
     },
-    /*
-    openDialog opens the dialog component by setting dialogIsOpen to true and setting the activeEntry so
-    the dialog component knows what data to display.
-
-    This allows us to use a single dialog component for all entries rather than creating a dialog component
-    for each entry.
-    */
     openDialog (date, time) {
       this.activeEntry = this.entries[date][time]
       this.dialogIsOpen = true
     },
-    /*
-    closeDialog closes the dialog by setting dialogIsOpen to false. Doing this will make Vue propogate that change
-    to they dialog component as a property (aka prop). That prop is used to set the styling on the component
-    to display: none; to hide it or display: block; to show it normally.
-    */
     closeDialog () {
       this.dialogIsOpen = false
     },
-    /*
-    addActivity adds an activity to the activeEntry. So if you look in the EntryDialog component you'll see there is a
-    button for adding entries. That button emits a click event that then calls a function which itself calls this.$emit('add')
-
-    We can call 'add' whatever we'd like, but we have to be consistent and handle that event on the component by using
-    :whatever-we-call-it="addActivity()"
-
-    addActivity makes sure that the activeEntry has a date and time before continuing. If there is a date and time, it
-    will add an activity by the name of 'Sleep' and has a duration of 20.
-    */
     addActivity () {
       if (!this.activeEntry.date || !this.activeEntry.time) {
         return
       }
+
+      // Don't add more activities than available
+      if (this.entries[this.activeEntry.date][this.activeEntry.time].activities.length >= this.defaultActivites.length) {
+        return
+      }
+
+      let availableName = this.getAvailableActivityName(this.entries[this.activeEntry.date][this.activeEntry.time].activities)
+
       this.entries[this.activeEntry.date][this.activeEntry.time].activities.push({
-        name: 'Sleep',
-        duration: 20
+        name: availableName,
+        isDone: false
       })
     },
-    /*
-    removeActivity removes an activity from the activeEntry for a given index. Since activities is an array we need an
-    index to remove something from it.
-    */
     removeActivity (index) {
       if (index > -1) {
         this.entries[this.activeEntry.date][this.activeEntry.time].activities.splice(index, 1)
       }
+    },
+    getAvailableActivityName (selected) {
+      let options = this.defaultActivites
+      selected = JSON.parse(JSON.stringify(selected))
+      selected = Object.keys(selected).map(key => selected[key].name) // e.g. ['Medicine', 'Sleep']
+
+      options = options.filter(i => selected.indexOf(i) < 0)
+      return options[0]
     }
   }
 }
--- src/components/EntryDialog.vue
+++ src/components/EntryDialog.vue
@@ -1,15 +1,4 @@
 <template lang="html">
-  <!--
-  v-on:click allows us to do something when the click event is emitted. In this case, when click is fired, we will
-  call our function called close() that's defined locally. We pass the $event because our close function wants to make sure that
-  we are clicking outside of the modal content.
-
-  :style is a way for us to pass data down to the property (or prop) called style. In this case it's basically allowing us
-  to define styles that is connected to our javascript. So in this code we are saying, if open is true then set display: block;
-  if open in false set display: 'none'..
-
-  display is a css property of course.
-  -->
   <div id="modal"
     v-on:click="close($event)"
     class="entry-dialog modal"
@@ -17,40 +6,19 @@
     <div class="modal-content-container">
       <div class="modal-content">
         <div class="activityList-container">
-          <!--
-          Loop through the activities of this entry and display them here. Notice how we're also interested in the index of the item
-          in the array. So we have "(activity, index) in entry.activities". Instead of "activity in entry.activities"
-          -->
           <div class="activityItem" v-for="(activity, index) in entry.activities">
             <div class="name-container">
-              <!--
-              This is a custom component from the library called KeenUI.
-
-              Documentation on how to use these components: https://josephuspaye.github.io/Keen-UI/#/ui-alert
-              -->
               <ui-select
                 v-model="activity.name"
+                v-on:select="setActivity()"
                 :placeholder="'Select and activity'"
-                :options="activityOptions"></ui-select>
+                :options="activityOptions[index]"></ui-select>
             </div>
-            <div class="duration-container">
-              <!--
-              Another KeenUI component. It comes with a bunch of styling that we can use for free. You can try creating
-              your own slider but this was faster to use someone else's
-              -->
-              <ui-slider
-                v-model="activity.duration"
-                :step="15"
-                :snap-to-steps="true"
-                :show-marker="true"
-                :marker-value="Math.floor((activity.duration / 100) * 60)"></ui-slider>
-              <span>{{Math.floor((activity.duration / 100) * 60)}} minutes</span>
+            <div class="input-container">
+              <ui-checkbox
+                v-model="activity.isDone"></ui-checkbox>
             </div>
             <div class="remove-btn-container">
-              <!--
-              Another KeenUI component. You can use <button></button> which is just an html element but it is styled
-              ugly. Using other people's code like this allows you to have styling out of the box with little effort.
-              -->
               <ui-button v-on:click="removeActivity(index)">Remove</ui-button>
             </div>
           </div>
@@ -64,31 +32,24 @@
 </template>
 
 <script>
-// Import the 3rd party components we'll use in our markup
 import UiSelect from './KeenUI/UiSelect'
-import UiSlider from './KeenUI/UiSlider'
+import UiCheckbox from './KeenUI/UiCheckbox'
 import UiButton from './KeenUI/UiButton'
 
-// Export this component so other components can use it
 export default {
-  // Name the component (required)
   name: 'sl-entry-dialog',
-  // Declare how you'd like to name the components in the markup
   components: {
     'ui-select': UiSelect,
-    'ui-slider': UiSlider,
+    'ui-checkbox': UiCheckbox,
     'ui-button': UiButton
   },
-  // Define the properties that any parent component can pass down this component (the child)
-  props: {
-    // open is a prop that is expected to be of type Boolean and have a default value of false (if the parent doesn't pass anything down)
+  props: { // props is data that is stored locally in this component but compes from the parent component..
     open: {
       type: Boolean,
       default () {
         return false
       }
     },
-    // entry is a prop that is expected to be an Object and have a default (if the parent doesn't pass anything)
     entry: {
       type: Object,
       default () {
@@ -97,63 +58,88 @@ export default {
           time: '',
           activities: [{
             name: 'Sleep',
-            duration: 30
+            isDone: false
           }]
         }
       }
+    },
+    defaultActivites: {
+      type: Array,
+      default () {
+        return []
+      }
     }
   },
-  /*
-  data() returns an object of the local state of this component. State is just data. it's just a representation of the
-  information that this component cares about. For instance activityOptions is part of this component's state.
-
-  You can add more data as you see fit. Normally it is data like strings, objects, arrays etc.. not functions.. those belong
-  in methods object
-  */
   data () {
-    return {
-      // activityOptions is used by out markup to generate the names of the select options
-      activityOptions: ['Sleep', 'Caffeine', 'Medicine']
+    return { }
+  },
+  computed: {
+    activityOptions () {
+      let activities = JSON.parse(JSON.stringify(this.entry.activities))
+      activities = Object.keys(activities).map(key => activities[key].name)
+
+      return this.calculateAvailableOptions(activities)
     }
   },
-  /*
-  methods allows us to write functions that we can use in our markup or even in lifecycle hooks like mounted() if we want to
-  do some setup or stuff that is specific to this component.
-  */
   methods: {
-    /*
-    close is a function that takes the event and determines if the id of the target is 'modal'. if the id is modal
-    then we know the user has clicked outside of the modal-content div. They have clicked in the dark area. So
-    we interpret that as them wanting to leave.
+    setActivity () {
+      let activities = JSON.parse(JSON.stringify(this.entry.activities))
+      activities = Object.keys(activities).map(key => activities[key].name)
 
-    We then emit an event that the parent can handle if they want. The event we emit is called 'close'. If the parent
-    component wants to know when we emit this event, they can use
+      let duplicate = this.checkForDuplicateOptions(activities)
+      if (duplicate.exists) {
+        console.log('removing')
+        this.removeActivity(duplicate.index)
+      }
+    },
+    checkForDuplicateOptions (activities) {
+      let options = {}
+      let duplicate = { exists: false, index: 0 }
 
-    v-on:close="doSomething()"
-    */
+      for (let i = 0; i < activities.length; i++) {
+        if (!options[activities[i]]) {
+          options[activities[i]] = true
+        } else {
+          duplicate.exists = true
+          duplicate.index = i
+        }
+      }
+
+      console.log(duplicate)
+
+      return duplicate
+    },
+    calculateAvailableOptions (activities) {
+      let options = []
+      let selected = []
+
+      if (!selected) {
+        return
+      }
+
+      for (let i = 0; i < activities.length; i++) {
+        options[i] = JSON.parse(JSON.stringify(this.defaultActivites)) || []
+        selected[i] = activities.slice(0, i) || []
+
+        options[i] = options[i].filter(arrIndex => selected[i].indexOf(arrIndex) < 0)
+      }
+
+      return options
+    },
     close (event) {
       if (event.target.id === 'modal') {
         this.$emit('close')
       }
     },
-    /*
-    addActivity is a function that we use to tell the parent that we want to add an activity to this current entry
-    */
     addActivity () {
       this.$emit('add')
     },
-    /*
-    removeActivity is a function that we use to tell the parent that we want to remove an activity with a given
-    index from the current entry.
-    */
     removeActivity (index) {
       this.$emit('remove', index)
     }
   }
 }
 </script>
-
-<!-- SASS/CSS code -->
 <style lang="scss" scoped>
 
 .entry-dialog {
@@ -164,12 +150,12 @@ export default {
   display: block;
   position: relative;
 
-  .name-container, .duration-container, .remove-btn-container {
+  .name-container, .input-container, .remove-btn-container {
     vertical-align: top;
     display: inline-block;
   }
 
-  .name-container, .duration-container {
+  .name-container, .input-container {
     width: 200px;
   }
 
@@ -178,7 +164,6 @@ export default {
   }
 }
 
-/* The Modal (background) */
 .modal {
     display: none; /* Hidden by default */
     position: fixed; /* Stay in place */
@@ -192,7 +177,6 @@ export default {
     background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
 }
 
-/* Modal Content/Box */
 .modal-content-container {
     background-color: #fefefe;
     margin: 15% auto; /* 15% from the top and centered */
